0bddef7442cc66f38ff59a5e5bbe86359dd0799c
JanBaranovic <jan.baranovic77@gmail.com>
2020-10-01 21:08:36 +0200

    all templates done

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 
+from .database import articles
+
 flask_app = Flask(__name__)
 
 @flask_app.route("/")
@@ -13,12 +15,26 @@ def view_about():
 
 @flask_app.route("/slovakia/")
 def view_slovakia():
-    return render_template("slovakia-articles.html")
+    return render_template("slovakia_articles.html", articles = articles.items())
+
+@flask_app.route("/slovakia/<int:art_id>/")
+def view_slovakia_article(art_id):
+    article = articles.get(art_id)
+    if article:
+        return render_template("slovakia_article.html", article = article)
+    return render_template("article_not_found.html", art_id = art_id)
 
 @flask_app.route("/world/")
 def view_world():
-    return render_template("world-articles.html")
+    return render_template("world_articles.html", articles = articles.items())
+
+@flask_app.route("/world/<int:art_id>/")
+def view_world_article(art_id):
+    article = articles.get(art_id)
+    if article:
+        return render_template("world_article.html", article = article)
+    return render_template("article_not_found.html", art_id = art_id)
 
 @flask_app.route("/contact/")
 def view_contact():
-    return render_template("contact")
+    return render_template("contact.html")
---
+++ mdblog/database.py
@@ -0,0 +1,18 @@
+articles = {
+    1 : {
+        'title' : "Velmi putavy clanok",
+        'content' : "Asi nieco o tom ako nas skoro medvede zchaluvali na veceru"
+    },
+    2 : {
+        'title' : "Dalsi velmi putavy clanok",
+        'content' : "Asi o tych jelenoch co nam skoro na hlavy poskakali"
+    },
+    3 : {
+        'title' : "Nie moc zaujimavy clanok",
+        'content' : "To bude asi nieco z bezneho zivotika ako skola a podobny grc. Jelly tiramisu chocolate cake candy canes wafer candy canes ice cream cheesecake. Caramels gingerbread marzipan croissant powder pastry tart. Gingerbread bonbon jelly beans chocolate cake carrot cake. Dessert cake gummi bears candy. Dessert toffee chocolate cake tart."
+    },
+    4 : {
+        'title' : "Tento je fajn celkom",
+        'content' : "Taka stara dobra klasika"
+    }
+}
